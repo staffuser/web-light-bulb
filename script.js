ac5d859303152e5719ff0e40fb1180f0980613b1
staffuser <119116574+staffuser@users.noreply.github.com>
2023-12-15 23:58:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//document.getElementById("demo").innerHTML="Hello World!";
+function changeImage() {
+    new Audio(scr='typing.wav').play();
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+    } else {
+        image.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+    }
+}
